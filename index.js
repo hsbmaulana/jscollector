a7cb83663d2f8276b1ca8319810163e9eff1a430
Hasbi Maulana <38196994+hsbmaulana@users.noreply.github.com>
2019-08-01 11:17:49 +0700

    Non-Linier Data Structure Created : Tree & Graph

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ module.exports.List =
     ArrayList  : require('./src/List/ArrayList'),
     LinkedList : require('./src/List/LinkedList'),
 };
+
+module.exports.Tree  = require('./src/Tree/Tree');
+module.exports.Graph = require('./src/Graph/Graph');
